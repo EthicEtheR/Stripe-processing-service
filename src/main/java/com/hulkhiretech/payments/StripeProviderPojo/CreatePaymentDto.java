e66cae461cba5546da9ea285b60def52e036652e
Akash Kannaujiya <ak04238@gmail.com>
2026-08-14 16:30:54 +0530

    minor changes in naming

--- src/main/java/com/hulkhiretech/payments/StripeProviderPojo/CreatePaymentDto.java
+++ src/main/java/com/hulkhiretech/payments/StripeProviderPojo/CreatePaymentDto.java
@@ -1,13 +1,11 @@
 package com.hulkhiretech.payments.StripeProviderPojo;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
-@Slf4j
-@Data
 
+@Data
 public class CreatePaymentDto {
     private String successUrl;
     private String cancelUrl;
